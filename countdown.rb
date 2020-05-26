026c11d4479de7d77512e9f43ea8401cb35ac75a
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 18:26:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,13 @@
 #write your code here
-
-def countdown(int)
+def countdown(number)
   i = number
   while i > 0
-    puts "#{number}"
+    if i == 1
+      puts "#{number} SECOND!"
+    else
+      puts "#{number} SECONDS!"
+    end
+    i -= 1
   end
+  "HAPPY NEW YEAR!"
 end
